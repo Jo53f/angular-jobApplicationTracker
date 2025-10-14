3c8c6f59382ac743a13ec2edb4b3f1203e6fb45a
josef <josef.abdulkarim@hotmail.co.uk>
2025-10-14 08:23:50 +0100

    replaced previous applications data list with material datasource

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,20 +1,34 @@
 import { Component, inject } from '@angular/core';
 import { JobApplicationInfo } from '../job-application-info';
 import { LoadApplications } from '../load-applications';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatTableModule, MatFormFieldModule, MatInputModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-  jobApplicationList: JobApplicationInfo[] = [];
+  // jobApplicationList: JobApplicationInfo[] = [];
   applicationsLoader: LoadApplications = inject(LoadApplications);
+  columnToDisplay = ['job_title', 'company']
+  dataSource = new MatTableDataSource(
+    this.applicationsLoader.load()
+  );
 
   constructor(){
-    this.jobApplicationList = this.applicationsLoader.load();
-    this.applicationsLoader.save(this.jobApplicationList);
+    // this.jobApplicationList = this.applicationsLoader.load();
+    // this.applicationsLoader.save(this.jobApplicationList);
+    // this.dataSource = new MatTableDataSource(this.jobApplicationList);
+  }
+
+  applyFilter(event: Event){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
   }
 }
